3ff75e872693f3c16c01d075f1e1fb78d7b8e8c0
florent417 <flole@live.dk>
2019-06-13 13:16:17 +0200

    Started solution

---
+++ ChargeLockerClasses/Control.Handout.cs
@@ -0,0 +1,67 @@
+﻿public class Control
+{
+    // Enum med tilstande ("states") svarende til tilstandsdiagrammet for klassen
+    private enum ChargerLockState
+    {
+        Available,
+        Locked,
+        DoorOpen
+    };
+
+
+    private string logFile = "logfile.txt"; // Navnet på systemets log-fil
+
+    // Eksempel på event handler for eventet "RFID Detected" fra tilstandsdiagrammet for klassen
+    private void RfidDetected(int id)
+    {
+        switch (state)
+        {
+        case LadeskabState.Available:
+            // Check for ladeforbindelse
+            if (charger.IsConnected())
+            {
+                door.LockDoor();
+                charger.StartCharge();
+                oldId = id;
+                using (var writer = File.AppendText(logFile))
+                {
+                    writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", id);
+                }
+
+                Console.Writeline("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.");
+                state = LadeskabState.Locked;
+            }
+            else
+            {
+                Console.Writeline("Din telefon er ikke ordentlig tilsluttet. Prøv igen.");
+            }
+
+            break;
+
+        case LadeskabState.DoorOpen:
+            // Ignore
+            break;
+
+        case LadeskabState.Locked:
+            // Check for correct ID
+            if (id == oldId)
+            {
+                charger.StopCharge();
+                door.UnlockDoor();
+                using (var writer = File.AppendText(logFile))
+                {
+                    writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", id);
+                }
+
+                Console.Writeline("Tag din telefon ud af skabet og luk døren");
+                state = LadeskabState.Available;
+            }
+            else
+            {
+                Console.Writeline("Forkert RFID tag");
+            }
+
+            break;
+        }
+    }
+}
---
+++ ChargeLockerClasses/Interfaces/IDoor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargeLockerClasses.Interfaces
+{
+    interface IDoor
+    {
+        event EventHandler Opened;
+        event EventHandler Closed;
+
+        void Open();
+        void Close();
+        void LockDoor();
+        void UnlockDoor();
+    }
+}
