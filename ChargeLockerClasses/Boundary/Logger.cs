0e90d96f171113c8499111fb6424113db70d6e94
florent417 <flole@live.dk>
2019-06-13 19:27:58 +0200

    Added logger implementation

---
+++ ChargeLockerClasses/Boundary/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChargeLockerClasses.Interfaces;
+
+namespace ChargeLockerClasses.Boundary
+{
+    public class Logger : ILogger
+    {
+        private string _fileName;
+
+        public Logger()
+        public void LogDoorLocked(string id)
+        {
+
+        }
+
+        public void LogDoorUnlocked(string id)
+        {
+
+        }
+    }
+}
