3bb2cb037171a4ac5f1946cafeccb7b88a33447a
florent417 <flole@live.dk>
2019-06-13 19:37:56 +0200

    logger impl done

--- ChargeLockerClasses/Boundary/Logger.cs
+++ ChargeLockerClasses/Boundary/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,21 @@ namespace ChargeLockerClasses.Boundary
 {
     public class Logger : ILogger
     {
-        private string _fileName;
+        private StreamWriter _writer;
 
-        public Logger()
+        public Logger(StreamWriter writer)
+        {
+            _writer = writer;
+        }
+
         public void LogDoorLocked(string id)
         {
-
+            _writer.WriteLine(DateTime.Now + $": Locker locked with RFID: {id}");
         }
 
         public void LogDoorUnlocked(string id)
         {
-
+            _writer.WriteLine(DateTime.Now + $": Locker unlocked with RFID: {id}");
         }
     }
 }
